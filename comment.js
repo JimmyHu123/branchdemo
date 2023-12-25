68218e98ca0941b02aff3206f19c8cdf8a6580db
Jimmy Wu <jay18859@gmail.com>
2023-12-25 19:43:10 +0800

    JS added repeated comment checking function

--- comment.js
+++ comment.js
@@ -1,13 +1,16 @@
 let button=document.getElementById("commentcount");
 let commentcount=0;
+let commentlist=[];
 
 button.addEventListener('click',function(){
     commentcount++;
 
     let newcomment=document.getElementById("newcomment").value;
-    if(newcomment==""||newcomment==" "){
+    if(newcomment==""||newcomment==" "||commentlist.includes(newcomment)==1){
         return ;
     }
+    commentlist.push(newcomment);
+    
     let content="<br>"+commentcount+": "+newcomment;
     let comment=document.getElementById("comment");
     comment.innerHTML+=content;
